9df166c12ae021723e23ed66e83fa3b7802f34e7
Eric-cyber-1024 <66725309+Eric-cyber-1024@users.noreply.github.com>
2024-02-09 16:59:46 +0000

    Update UI style

--- 程序/PYQT5/Camera_view.py
+++ 程序/PYQT5/Camera_view.py
@@ -9,16 +9,70 @@ from enum import Enum
 import numpy as np
 
 display_monitor= 0
-NUM_BUTTON_WIDTH = 100
+NUM_BUTTON_WIDTH = 200
 NUM_BUTTON_HEIGHT = 100
+DEBUG = True
 
+BUTTON_STYLE = """
+        QPushButton {
+            background-color: White; 
+            color : Black;
+            border-width: 4px;
+            border-radius: 20px;
+        }
+        QPushButton:hover {
+            color: white;
+            background-color: darkgreen
+        }
+    """ 
+BUTTON_STYLE_RED = """
+        QPushButton {
+            background-color: Gray; 
+            color : white;
+            border-width: 4px;
+            border-radius: 20px;
+        }
+        QPushButton:hover {
+            color: white;
+            background-color: red
+        }
+    """ 
+SLIDER_STYLE = """
+                            .QSlider {
+                    min-height: 68px;
+                    max-height: 68px;
+               
+                }
+
+                .QSlider::groove:horizontal {
+                 
+                    height: 5px;
+                    background: white;
+                    margin: 0 12px;
+                    border-width: 10px;
+                        border-radius: 20px;
+                }
+
+                .QSlider::handle:horizontal {
+                    background: Darkgreen;
+                    width: 23px;
+                    height: 100px;
+                    margin: -24px -12px;
+                    border-width: 4px;
+                    border-radius: 20px;
+                }
+                            }
+        """
+
+BUTTON_FONT = QFont("Arial",40)
 
 class VideoThread(QThread):
     change_pixmap_signal = pyqtSignal(np.ndarray)
 
     def run(self):
         # capture from web cam
-        cap = cv2.VideoCapture(0)
+        cap = cv2.VideoCapture(2)
+        
         cap.set(3, 1920)
         cap.set(4, 1080)
         video_width = int(cap.get(3))
@@ -44,47 +98,80 @@ class Frequency_Selection(Enum):
 class App(QWidget):
     def __init__(self):
         super().__init__()
+        if (QDesktopWidget().screenCount() >1):
+            self.ScreenNumber = 1
+        else:
+            self.ScreenNumber = 0
         self.setWindowTitle("ISD Mic array Project")
-        self.disply_width = 1870
-        self.display_height = 1080
+        if DEBUG == True:
+            self.disply_width = 1600
+            self.display_height = 1080
+        else:
+            self.disply_width = 1920
+            self.display_height = 800
         self.LPF_Select = Frequency_Selection.LPF_FULL.value
         self.ButtonText = QFont("Arial", 15)
         # create the label that holds the image
         self.image_label = QLabel(self)
-        self.image_label.resize(self.disply_width, self.display_height)
-        self.image_label.setFixedSize(1920,800)
+        self.image_label.setText("Loading Camera Frame ...")
+        self.image_label.setStyleSheet("")
+        self.image_label.setFont(QFont("Arial",40))
+        # self.image_label.resize(self.disply_width, self.display_height)
+        if DEBUG == True:         
+            self.image_label.setFixedSize(1600,800)
+        else:
+            self.image_label.setFixedSize(1600,800)
+        self.image_label.setStyleSheet("background-color:Grey;border-width: 4px;border-radius: 20px;")
 
         self.RECORDING = False
         
 
         self.stacked_widget = QStackedWidget()
+        self.stacked_widget.setContentsMargins(0, 0, 0, 0)  # Set margins to zero
+         
         self.stacked_widget.setFixedSize(1920,1080)
+        # self.setStyleSheet("background: qlineargradient(x1:0, y1:0, x2:1, y2:1, stop:0 green, stop:1 white);")
+        self.setStyleSheet("background-color:lightgreen")
+        
         # create a text label
         self.index = APP_PAGE.MAIN.value
 
 
         self.layout = QVBoxLayout()
         self.layout.addWidget(self.stacked_widget)
+        self.layout.setContentsMargins(0,0,0,0)
+      
+
+     
 
         self.ExitButton = QPushButton()
         self.ExitButton.clicked.connect(self.Exit)
         self.ExitButton.setFixedSize(NUM_BUTTON_WIDTH,NUM_BUTTON_HEIGHT)
         self.ExitButton.setText("Exit")
-        self.ExitButton.setFont(self.ButtonText)
-        self.ExitButton.setStyleSheet("background-color:white;color:black ;border-width: 4px;border-radius: 20px;")
+        self.ExitButton.setFont(BUTTON_FONT )
+        self.ExitButton.setStyleSheet(BUTTON_STYLE)
 
         self.SettingButton = QPushButton()
         self.SettingButton.clicked.connect(self.switchPage)
         self.SettingButton.setFixedSize(NUM_BUTTON_WIDTH,NUM_BUTTON_HEIGHT)
         self.SettingButton.setText("Setting")
-        self.SettingButton.setFont(self.ButtonText)
-        self.SettingButton.setStyleSheet("background-color:white;color:black ;border-width: 4px;border-radius: 20px;")
+        self.SettingButton.setFont(BUTTON_FONT)
+        self.SettingButton.setStyleSheet(BUTTON_STYLE)
 
+
+        self.text_label =QPlainTextEdit()
+        self.text_label.setFixedSize(250,800)
+        self.text_label.setStyleSheet("background-color: white ;")
 
         #Setting Up Main Page 
         self.MainPage = QGridLayout()
+        self.MainPage.setHorizontalSpacing(0)  # Set horizontal spacing to zero   
+        self.MainPage.setVerticalSpacing(0)
         # self.MainPage = QVBoxLayout()
         self.MainPage.addWidget(self.image_label,0,0)
+        if DEBUG == True:
+            self.MainPage.addWidget(self.text_label,0,1)
+        
         # self.MainPage.addWidget(self.image_label)
         
 
@@ -92,8 +179,8 @@ class App(QWidget):
         self.RecordButton.clicked.connect(self.Record_clicked)
         self.RecordButton.setFixedSize(NUM_BUTTON_WIDTH,NUM_BUTTON_HEIGHT)
         self.RecordButton.setText("Record")
-        self.RecordButton.setFont(self.ButtonText)
-        self.RecordButton.setStyleSheet("background-color:white;color:black ;border-width: 4px;border-radius: 20px;")
+        self.RecordButton.setFont(BUTTON_FONT )
+        self.RecordButton.setStyleSheet(BUTTON_STYLE_RED)
 
         # self.MainPage.addWidget(self.textLabel,1,2,1,2)
 
@@ -104,8 +191,15 @@ class App(QWidget):
 
         self.MainPage_button_widget = QWidget()
         self.MainPage_button_widget.setLayout(self.MainPage_button)
-
-        self.MainPage.addWidget(self.MainPage_button_widget,1,0)
+        self.MainPage_button_widget.setStyleSheet("background-color:lightgreen")
+        self.MainPage_button_widget.setFixedSize(1820,180)
+        if DEBUG == True:
+            self.MainPage.addWidget(self.MainPage_button_widget,1,0,1,2)
+        else:
+            self.MainPage.addWidget(self.MainPage_button_widget,1,0)
+             
+        self.MainPage.setHorizontalSpacing(0)  # Set horizontal spacing to zero   
+        self.MainPage.setVerticalSpacing(0)  # Set horizontal spacing to zero   
 
 
         # self.MainPage.addWidget(self.ExitButton,1,0,3,1)
@@ -117,61 +211,89 @@ class App(QWidget):
 
         #Setting up Setting page for LPF and Gain and Voulme 
         self.SettingPage = QGridLayout()
+        
 
-        self.GainLabel = QLabel("Mic Array Channel Gain")
-        self.GainLabel.setFont(self.ButtonText)
+        self.GainLabel = QLabel("Mic Array Channel Gain  :")
+        self.GainLabel.setFont(BUTTON_FONT)
         self.GainFader=QSlider(Qt.Horizontal)
         self.GainFader.setMinimum(-12)
         self.GainFader.setMaximum(12)
         self.GainFader.setValue(0)
         self.GainFader.setTickPosition(QSlider.TicksBelow)
-        self.GainFader.setTickInterval(3)
+        self.GainFader.setTickInterval(1)
+        self.GainFader.setFont(QFont("Arial",40))
+        self.GainFader.setStyleSheet(SLIDER_STYLE)
 
-        self.VolumeLabel = QLabel("Mic Array Digital Volume")
-        self.VolumeLabel.setFont(self.ButtonText)
+        self.VolumeLabel = QLabel("Mic Array Digital Volume :")
+        self.VolumeLabel.setFont(BUTTON_FONT)
 
         self.VolumeFader=QSlider(Qt.Horizontal)
         self.VolumeFader.setMinimum(0)
         self.VolumeFader.setMaximum(24)
         self.VolumeFader.setValue(0)
         self.VolumeFader.setTickPosition(QSlider.TicksBelow)
-        self.VolumeFader.setTickInterval(3)
+        self.VolumeFader.setTickInterval(1)
+        self.VolumeFader.setFont(QFont("Arial",40))
+        self.VolumeFader.setStyleSheet(SLIDER_STYLE)
 
-        self.FilterSelectLabel = QLabel("Mic Array Filter Select")
-        self.FilterSelectLabel.setFont(self.ButtonText)
+
+        self.FilterSelectLabel = QLabel("Mic Array Filter Select     :")
+        self.FilterSelectLabel.setFont(BUTTON_FONT)
 
 
         self.CheckBox_6kHz = QCheckBox('6kHz') 
         self.CheckBox_6kHz.setGeometry(200, 150, 100, 30) 
         self.CheckBox_6kHz.setChecked(False) 
+        self.CheckBox_6kHz.setFont(BUTTON_FONT)
+        self.CheckBox_6kHz.setStyleSheet("""QCheckBox::indicator {
+                                            width: 40px;
+                                            height: 40px;
+                                        }""")
         # self.CheckBox_6kHz.stateChanged.connect(lambda:self.ToggleSelection(Frequency_Selection.LPF_6K.value))        
 
 
         self.CheckBox_12kHz = QCheckBox('12kHz') 
         self.CheckBox_12kHz.setGeometry(200, 150, 100, 30) 
         self.CheckBox_12kHz.setChecked(False) 
+        self.CheckBox_12kHz.setFont(BUTTON_FONT)
+        self.CheckBox_12kHz.setStyleSheet("""QCheckBox::indicator {
+                                            width: 40px;
+                                            height: 40px;
+                                        }""")
         # self.CheckBox_12kHz.stateChanged.connect(lambda:self.ToggleSelection(Frequency_Selection.LPF_12K.value))   
         #
 
 
         self.CheckBox_18kHz = QCheckBox('18kHz') 
         self.CheckBox_18kHz.setGeometry(200, 150, 100, 30) 
-        self.CheckBox_18kHz.setChecked(True) 
+        self.CheckBox_18kHz.setChecked(False) 
+        self.CheckBox_18kHz.setFont(BUTTON_FONT)
+        self.CheckBox_18kHz.setStyleSheet("""QCheckBox::indicator {
+                                            width: 40px;
+                                            height: 40px;
+                                        }""")
         # self.CheckBox_18kHz.stateChanged.connect(lambda:self.ToggleSelection(Frequency_Selection.LPF_18K.value))   
    
 
         self.CheckBox_Full = QCheckBox('Full Range') 
         self.CheckBox_Full.setGeometry(200, 150, 100, 30) 
         self.CheckBox_Full.setChecked(True) 
+        self.CheckBox_Full.setFont(BUTTON_FONT)
+        self.CheckBox_Full.setStyleSheet("""QCheckBox::indicator {
+                                            width: 40px;
+                                            height: 40px;
+                                        }""")
         # self.CheckBox_Full.stateChanged.connect(lambda:self.ToggleSelection(Frequency_Selection.LPF_FULL.value))   
 
         self.BackButton = QPushButton()
         self.BackButton.clicked.connect(self.switchPage)
-        self.BackButton.setFixedSize(NUM_BUTTON_WIDTH,NUM_BUTTON_HEIGHT)
+        self.BackButton.setFixedSize(NUM_BUTTON_WIDTH+100,NUM_BUTTON_HEIGHT)
         self.BackButton.setText("Back")
         self.BackButton.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         self.BackButton.setFont(self.ButtonText)
-
+        self.BackButton.setStyleSheet(BUTTON_STYLE )
+        self.BackButton.setFont(QFont("Arial",40))
+        
 
         
 
@@ -215,7 +337,7 @@ class App(QWidget):
  
 
         # self.Monitor = QDesktopWidget().screenGeometry(display_monitor)
-        self.setGeometry(QApplication.screens()[0].geometry())
+        self.setGeometry(QApplication.screens()[self.ScreenNumber].geometry())
         # self.setGeometry(self.Monitor.left(),self.Monitor.right())
         self.showFullScreen()
         # create the video capture thread
@@ -250,10 +372,10 @@ class App(QWidget):
         for i in range (len(list_of_range)):
                 if i != choice:
                     list_of_range[i].setChecked(False)
-                    list_of_range[i].setEnabled(True)
+                    # list_of_range[i].setEnabled(True)
                 else:
                      list_of_range[i].setChecked(True)
-                     list_of_range[i].setEnabled(False)
+                    #  list_of_range[i].setEnabled(False)
                 # else:
                     # list_of_range[i].setChecked(False)
     
@@ -261,8 +383,11 @@ class App(QWidget):
         self.RECORDING = not self.RECORDING
         if self.RECORDING == True:
             self.RecordButton.setStyleSheet("background-color:red ; color :white ;border-width: 4px;border-radius: 20px;")
+
+            self.text_label.appendPlainText("Recording")
         else:
             self.RecordButton.setStyleSheet("background-color:white;color:black ;border-width: 4px;border-radius: 20px;")
+            self.text_label.appendPlainText("Stopped Recording")   
                   
 
     def switchPage(self):
